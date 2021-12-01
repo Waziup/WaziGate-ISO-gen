e55b266e3225e1c7f296ccd9d07546eb207bb456
Johann Forster <25847959+j-forster@users.noreply.github.com>
2021-12-01 13:32:58 +0100

    Fix libseccomp2 issue, Cleanup
    - Add WAZIGATE_TAG to setup.sh
    - Remove tar files after docker import
    - Add libseccomp2 to fix timing issues
    - Change docker log driver to "local"

--- stage3/02-wazigate/00-run.sh
+++ stage3/02-wazigate/00-run.sh
@@ -33,6 +33,17 @@ sed -i "s/^WAZIGATE_TAG.*/WAZIGATE_TAG=$WAZIGATE_TAG/g" $WAZIGATE_DIR/.env
 ################################################################################
 
 
+# Install newer version for libseccomp2
+echo 'deb http://httpredir.debian.org/debian buster-backports main contrib non-free' | sudo tee -a "$ROOTFS_DIR/etc/apt/sources.list.d/debian-backports.list"
+on_chroot <<EOF
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 648ACFD622F3D138
+apt-get update
+apt-get install -y -qq --no-install-recommends -t buster-backports libseccomp2
+EOF
+
+################################################################################
+
+
 if [ "${CLEAN}" = "1" ]; then
   rm -rf files/*.tar
 fi
@@ -53,7 +64,7 @@ function install_docker_image {
   fi
 
   # Copy Docker Images
-  install -m 644 files/$1.tar  $WAZIGATE_DIR/
+  install -m 644 files/$1.tar "$WAZIGATE_DIR/"
 }
 # WaziGate Core (WaziGate-Edge and MongoDB)
 # install_docker_image "wazigate-mongo" "webhippie/mongodb:latest"
@@ -66,7 +77,7 @@ install_docker_image "chirpstack-network-server" "waziup/chirpstack-network-serv
 install_docker_image "chirpstack-application-server" "waziup/chirpstack-application-server:3.13.2"
 install_docker_image "chirpstack-gateway-bridge" "waziup/chirpstack-gateway-bridge:3.9.2"
 install_docker_image "postgresql" "waziup/wazigate-postgresql"
-install_docker_image "redis" "redis:5-alpine"
+install_docker_image "redis" "redis:6-alpine"
 install_docker_image "wazigate-lora-forwarders" "waziup/wazigate-lora-forwarders"
 
 ################################################################################
@@ -75,27 +86,25 @@ install_docker_image "wazigate-lora-forwarders" "waziup/wazigate-lora-forwarders
 # Setup Work
 
 rm -f "$ROOTFS_DIR/etc/systemd/system/dhcpcd.service.d/wait.conf"
-mv --backup=numbered $ROOTFS_DIR/etc/dnsmasq.conf $ROOTFS_DIR/etc/dnsmasq.conf.orig
-echo 'interface=wlan0\n  dhcp-range=192.168.200.2,192.168.200.200,255.255.255.0,24h\n' > $ROOTFS_DIR/etc/dnsmasq.conf
+mv --backup=numbered "$ROOTFS_DIR/etc/dnsmasq.conf" "$ROOTFS_DIR/etc/dnsmasq.conf.orig"
+echo 'interface=wlan0\n  dhcp-range=192.168.200.2,192.168.200.200,255.255.255.0,24h\n' > "$ROOTFS_DIR/etc/dnsmasq.conf"
 
-cp files/hostapd.conf $ROOTFS_DIR/etc/hostapd/hostapd.conf
+cp files/hostapd.conf "$ROOTFS_DIR/etc/hostapd/hostapd.conf"
 
-if ! grep -qFx 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' $ROOTFS_DIR/etc/default/hostapd; then
-  sed -i -e '$i \DAEMON_CONF="/etc/hostapd/hostapd.conf"\n' $ROOTFS_DIR/etc/default/hostapd
+if ! grep -qFx 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' "$ROOTFS_DIR/etc/default/hostapd"; then
+  sed -i -e '$i \DAEMON_CONF="/etc/hostapd/hostapd.conf"\n' "$ROOTFS_DIR/etc/default/hostapd"
 fi
 
-cp --backup=numbered $ROOTFS_DIR/etc/wpa_supplicant/wpa_supplicant.conf $ROOTFS_DIR/etc/wpa_supplicant/wpa_supplicant.conf.orig
+cp --backup=numbered $ROOTFS_DIR/etc/wpa_supplicant/wpa_supplicant.conf "$ROOTFS_DIR/etc/wpa_supplicant/wpa_supplicant.conf.orig"
 
-echo "static domain_name_servers=8.8.8.8" >> $ROOTFS_DIR/etc/dhcpcd.conf
-
-# cp setup/clouds.json wazigate-edge
+echo "static domain_name_servers=8.8.8.8" >> "$ROOTFS_DIR/etc/dhcpcd.conf"
 
 # Run setup.sh as systemd service on boot  
-cp $WAZIGATE_DIR/wazigate-setup.service $ROOTFS_DIR/etc/systemd/system
+mv "$WAZIGATE_DIR/wazigate-setup.service" "$ROOTFS_DIR/etc/systemd/system"
 
 # Run wazigate-host as systemd service on boot  
-cp $WAZIGATE_DIR/wazigate-host/wazigate-host $ROOTFS_DIR/usr/bin
-cp $WAZIGATE_DIR/wazigate-host/wazigate-host.service $ROOTFS_DIR/etc/systemd/system
+mv "$WAZIGATE_DIR/wazigate-host/wazigate-host" "$ROOTFS_DIR/usr/bin"
+mv "$WAZIGATE_DIR/wazigate-host/wazigate-host.service" "$ROOTFS_DIR/etc/systemd/system"
 
 
 # Enable Wazigate services
@@ -104,13 +113,16 @@ systemctl enable wazigate-setup
 systemctl enable wazigate-host
 EOF
 
-# Enable SPI
-# echo -e '\ndtparam=i2c_arm=on' >> $ROOTFS_DIR/boot/config.txt
-
-# Enable I2C
-# echo ' bcm2708.vc_i2c_override=1' >> $ROOTFS_DIR/boot/cmdline.txt
-# echo -e '\ni2c-bcm2708' >> $ROOTFS_DIR/etc/modules-load.d/raspberrypi.conf
-# echo -e '\ni2c-dev' >> $ROOTFS_DIR/etc/modules-load.d/raspberrypi.conf
+# 
+cat <<EOT >> $ROOTFS_DIR/etc/sysctl.conf
+###################################################################
+# Change the kernel virtual memory accounting mode.
+#   Values are:
+#     0: heuristic overcommit (this is the default)
+#     1: always overcommit, never check
+#     2: always check, never overcommit
+vm.overcommit_memory = 1
+EOT
 
 # Show text-ui on login
-echo -e "sudo bash /var/lib/wazigate/wazigate-host/text-ui.sh" >> $ROOTFS_DIR/home/$FIRST_USER_NAME/.profile
+echo -e "sudo bash /var/lib/wazigate/wazigate-host/text-ui.sh" >> "$ROOTFS_DIR/home/$FIRST_USER_NAME/.profile"
