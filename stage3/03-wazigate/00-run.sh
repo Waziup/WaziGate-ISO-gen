2de56f951d8647634f02406c8cb6ed22f6ea65b0
Felix Markwordt <24inchstreetbikesoldier@gmail.com>
2022-05-30 17:31:10 +0200

    solved issue with redis and log2ram

--- stage3/03-wazigate/00-run.sh
+++ stage3/03-wazigate/00-run.sh
@@ -13,12 +13,18 @@ EOF
 # Overwrite mongodb service file on host
 install -m 644 files/mongod.service "$ROOTFS_DIR/lib/systemd/system/"
 
-# Overwrite redis.conf file on host and make folder for socket file, change owner and group and replace redis.service file
+# Overwrite redis.conf file on host 
 install -m 644 files/redis.conf "$ROOTFS_DIR/etc/redis/"
-mkdir "$ROOTFS_DIR/var/run/redis"
-chown redis:redis "$ROOTFS_DIR/var/run/redis"
+# Make folder for socket file, change owner and group, on_chroot: because no user redis
+on_chroot <<EOF
+install -d -m 0755 -o redis -g redis "$ROOTFS_DIR/var/run/redis/"
+EOF
+#mkdir "$ROOTFS_DIR/var/run/redis"
+#on_chroot <<EOF
+#chown redis:redis "$ROOTFS_DIR/var/run/redis/"
+#EOF
+# Replace redis.service file
 install -m 644 files/redis.service "$ROOTFS_DIR/etc/systemd/system/"
-
 
 # Install Network Manager config
 install -m 644 files/NetworkManager.conf "$ROOTFS_DIR/etc/NetworkManager/"
@@ -47,11 +53,22 @@ EOF
 apt install rsync
 
 #Install Log2RAM and copy configuration
-wget https://github.com/azlux/log2ram/archive/master.tar.gz -O "$ROOTFS_DIR/home/$FIRST_USER_NAME/log2ram.tar.gz"
-tar xf "$ROOTFS_DIR/home/$FIRST_USER_NAME/log2ram.tar.gz"
-bash "$ROOTFS_DIR/home/$FIRST_USER_NAME/log2ram-master/install.sh"
-rm -f "$ROOTFS_DIR/home/$FIRST_USER_NAME/log2ram.tar.gz"
-rm -rf "$ROOTFS_DIR/home/$FIRST_USER_NAME/log2ram-master"
+# wget https://github.com/azlux/log2ram/archive/master.tar.gz -O "$ROOTFS_DIR/home/$FIRST_USER_NAME/log2ram.tar.gz"
+# tar -xf "$ROOTFS_DIR/home/$FIRST_USER_NAME/log2ram.tar.gz"
+# bash "$ROOTFS_DIR/home/$FIRST_USER_NAME/log2ram-master/install.sh"
+# rm -f "$ROOTFS_DIR/home/$FIRST_USER_NAME/log2ram.tar.gz"
+# rm -rf "$ROOTFS_DIR/home/$FIRST_USER_NAME/log2ram-master"
+
+echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ bullseye main" | sudo tee "$ROOTFS_DIR/etc/apt/sources.list.d/azlux.list"
+sudo wget -O "$ROOTFS_DIR/usr/share/keyrings/azlux-archive-keyring.gpg"  https://azlux.fr/repo.gpg
+
+## sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CA548A0A0312D8E6
+on_chroot <<EOF
+
+sudo apt update
+sudo apt install log2ram
+EOF
+
 install -m 644 files/log2ram.conf "$ROOTFS_DIR/etc/"
 
 # Show text-ui on login
@@ -64,8 +81,7 @@ systemctl enable wazigate-host
 
 systemctl enable mongod
 systemctl enable wazigate
-systemctl enable redis-server
 EOF
 
-# Create log file for setup
+# Create log file for wazigate-setup
 touch "$ROOTFS_DIR/tmp/wazigate-setup-step.txt"
