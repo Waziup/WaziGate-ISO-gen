23e2b5b6e553f1358a083ccfbbd8bc9081190610
Felix Markwordt <24inchstreetbikesoldier@gmail.com>
2022-06-17 15:28:31 +0200

    changed path for mongodb-org

--- stage3/01-install-packages/00-run.sh
+++ stage3/01-install-packages/00-run.sh
@@ -3,11 +3,11 @@
 # add for mongodb source location
 on_chroot <<EOF 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
-EOF
+
 
 echo \
-"deb [ arch=arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee "$ROOTFS_DIR/etc/apt/sources.list.d/mongodb-org-4.4.list"
+"deb [ arch=arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee "/etc/apt/sources.list.d/mongodb-org-4.4.list"
 
-on_chroot <<EOF
+
 apt-get update
 EOF
